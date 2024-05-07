13010953e061db019229472281edf2cb62ac2f33
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-05-07 19:20:36 +0530

    worked on footer

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -5,40 +5,38 @@ import logo from "/saudade.svg"
 
 const Header = () => {
   return (
-    <div>
-      <nav className="navbar">
-        <h1>
-          <NavLink to="/">
-            <img src={logo} alt="logo" className="logo" />
-          </NavLink>
-        </h1>
-        <ul>
-          <li>
-            <NavLink to="/#features" className="nav_links">
-              Features
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/pricing" className="nav_links">
-              Pricing
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" className="nav_links">
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" className="nav_links">
-              Contact Us
-            </NavLink>
-          </li>
-        </ul>
-        <NavLink to="/login">
-          <button className="login_btn">Login</button>
+    <nav className="navbar">
+      <h1>
+        <NavLink to="/">
+          <img src={logo} alt="logo" className="logo" />
         </NavLink>
-      </nav>
-    </div>
+      </h1>
+      <ul>
+        <li>
+          <NavLink to="/#features" className="nav_links">
+            Features
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing" className="nav_links">
+            Pricing
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="nav_links">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className="nav_links">
+            Contact Us
+          </NavLink>
+        </li>
+      </ul>
+      <NavLink to="/login">
+        <button className="login_btn">Login</button>
+      </NavLink>
+    </nav>
   )
 }
 
