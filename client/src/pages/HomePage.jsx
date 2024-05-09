f92c189ce38faa8a4d47f1d4bd8a43bc1e29a16b
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-05-09 18:22:49 +0530

    worked on hero section

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,25 @@
 import React from "react"
 import "./styles/HomePage.css"
 import Layout from "../components/Layout/Layout"
-import HomepageBackground from "../components/Backgrounds/HomepageBackground"
-import ProfileCard from "../components/ProfileCard/ProfileCard"
 
 const HomePage = () => {
   return (
     <Layout>
       <div className="homepage">
-        <div className="profile_card_homepage">
-          <ProfileCard />
+        <div className="tagline_container">
+          <div className="main_tagline">
+            Never get <span className="gradient_main_tagline">stu...ck,</span>{" "}
+            <br /> while{" "}
+            <span className="gradient_main_tagline">uploa...ding!</span>
+          </div>
+          <div className="sub_tagline">
+            Uploading files has never been easier! Just drag and drop your files and we'll take care of the rest.
+          </div>
         </div>
-        <div className="nothing">Tryna implement a different background for uniqueness</div>
+        <div className="hero_buttons_container">
+          <button className="hero_button_get">Get Started</button>
+          <button className="hero_button_learn">Learn More</button>
+        </div>
       </div>
     </Layout>
   )
